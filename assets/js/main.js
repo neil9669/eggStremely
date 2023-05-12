0e4d8a709b2a2b0f590e374e52dbbb64c5abd438
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-05-12 17:04:49 +0100

    Set up kaboom and style

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// initialize kaboom context
+kaboom({
+    background: [0, 0, 255, ],
+    fullscreen: true,
+    scale: 1,
+    debug: true,
+})
+
+// add page title
+add([
+    text('eggStremely'),
+    pos(650, 50)
+]);
