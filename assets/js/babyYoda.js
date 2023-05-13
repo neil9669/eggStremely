e7dbb6cef377b609877ebbe27f0c894e391c4f61
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-05-14 00:41:49 +0100

    Create baby yoda file and add code

---
+++ assets/js/babyYoda.js
@@ -0,0 +1,14 @@
+
+export const babyYodaMoves = () => {
+
+    const babyYoda = add([
+
+        // Create Baby Yoda character
+        sprite("babyYoda"),
+        scale(2),
+        pos(width() * 0.5, height() * 0.5),
+        origin('center'),
+        "babyYoda",
+        ]);
+
+        }
