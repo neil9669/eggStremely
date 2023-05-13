896a64f5a46d4ade8cbd8423756728896a19bc8e
Stephen Dawson <stephendawson@protonmail.com>
2023-05-13 17:03:22 +0100

    import generateRandomPosition function into eggJar and enemy.js and update spawn code, remove function and unnessary imports from main.js, move utils folder into js folder

--- assets/js/enemy.js
+++ assets/js/enemy.js
@@ -1,10 +1,12 @@
+import generateRandomPosition from "./utils/generateRandomPosition.js";
 /**
  * creates an enemy object - the function is
  * imported and called in the main.js file
  */
 
-const spawnEnemy = (randPosX, randPosY, enemySprite) => {
-  const ENEMY_SIZE = 40;
+const spawnEnemy = (enemySprite) => {
+  const [randPosX, randPosY] = generateRandomPosition(); // using array destructuring assign variables to the return values
+  const ENEMY_SIZE = 40; // temporary enemy size until we have a sprite
   const enemy = add([
     sprite(enemySprite),
     pos(randPosX, randPosY), // random position passed in on main.js
---
+++ assets/js/utils/generateRandomPosition.js
@@ -0,0 +1,21 @@
+/**
+ * function to generate a random position that is
+ * a set distance away from the player
+ */
+const generateRandomPosition = (playerInitialXPos, playerInitialYPos) => {
+  const randomXPos = Math.floor(Math.random() * width());
+  const randomYPos = Math.floor(Math.random() * height());
+  const distanceFromPlayer = 200;
+  if (
+    randomXPos > playerInitialXPos - distanceFromPlayer &&
+    randomXPos < playerInitialXPos + distanceFromPlayer &&
+    randomYPos > playerInitialYPos - distanceFromPlayer &&
+    randomYPos < playerInitialYPos + distanceFromPlayer
+  ) {
+    return generateRandomPosition(playerInitialXPos, playerInitialYPos);
+  } else {
+    return [randomXPos, randomYPos];
+  }
+};
+
+export default generateRandomPosition;
