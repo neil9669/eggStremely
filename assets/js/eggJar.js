a1e8f49adf4de5be2f78ba7d854cf97bc1c7889c
Stephen Dawson <stephendawson@protonmail.com>
2023-05-13 22:22:16 +0100

    move sprites to public folder, add sprites to enemies and egg jar

--- assets/js/eggJar.js
+++ assets/js/eggJar.js
@@ -4,9 +4,8 @@
  */
 const spawnEggJar = (randPosX, randPosY, colour) => {
   const eggJar = add([
-    rect(40, 40), // placeholder until we have a sprite
+    sprite("egg-jar"),
     pos(randPosX, randPosY),
-    color(colour), // colour of the box until we have a sprite
     "egg-jar", // tag to reference later on
   ]);
 };
