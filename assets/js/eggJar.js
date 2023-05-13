13a8f7493b4e915b648faabe89adfbce7e69433c
Stephen Dawson <stephendawson@protonmail.com>
2023-05-13 14:09:52 +0100

    add additional comments to file for clarity

--- assets/js/eggJar.js
+++ assets/js/eggJar.js
@@ -4,9 +4,9 @@
  */
 const spawnEggJar = (randPosX, randPosY, colour) => {
   const eggJar = add([
-    rect(40, 40),
+    rect(40, 40), // placeholder until we have a sprite
     pos(randPosX, randPosY),
-    color(colour),
+    color(colour), // colour of the box until we have a sprite
     "egg-jar", // tag to reference later on
   ]);
 };
