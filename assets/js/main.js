ef470067148e944d4efa7b9be3a116b6e4d936a6
Stephen Dawson <stephendawson@protonmail.com>
2023-05-14 22:16:50 +0100

    remove unnessary code from main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,6 @@ kaboom({
   canvas: document.getElementById("main-game-canvas"), // select canvas element
   fullscreen: true,
   scale: 1,
-  debug: true,
 });
 
 loadRoot("../../public/sprites/");
@@ -23,37 +22,6 @@ loadSprite("background-door", "background-door.png");
 generateScenes();
 
 go("welcome");
-
-const playerInitialXPos = width() / 10;
-const playerInitialYPos = height() - height() / 5;
-
-// add player sprite - player sprite will just
-// be a square until we can add later
-const player = add([
-  rect(40, 40),
-  pos(playerInitialXPos, playerInitialYPos),
-  color(255, 0, 0),
-  "player",
-]);
-
-// set the initial player speed
-let playerSpeed = 120;
-
-keyDown("left", () => {
-  player.move(-playerSpeed, 0);
-});
-
-keyDown("right", () => {
-  player.move(playerSpeed, 0);
-});
-
-keyDown("up", () => {
-  player.move(0, -playerSpeed);
-});
-
-keyDown("down", () => {
-  player.move(0, playerSpeed);
-});
 
 // canvas focus enables user keyboard input register
 canvas.focus();
