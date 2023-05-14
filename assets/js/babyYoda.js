d9af75d0e4a775b69c2255a1e6ef500797121ffc
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-05-14 03:08:06 +0100

    Update babyyoda file to handle keyboard input

--- assets/js/babyYoda.js
+++ assets/js/babyYoda.js
@@ -1,14 +1,41 @@
 
-export const babyYodaMoves = () => {
-
+const spawnBabyYoda = (randPosX, randPosY, colour) => {
     const babyYoda = add([
+      rect(40, 40), // placeholder until we have a sprite
+      pos(randPosX, randPosY),
+      color(colour), // colour of the box until we have a sprite
+      "babyYoda", // tag to reference later on
+    ]);
 
-        // Create Baby Yoda character
-        sprite("babyYoda"),
-        scale(2),
-        pos(width() * 0.5, height() * 0.5),
-        origin('center'),
-        "babyYoda",
-        ]);
+        //check input on each frame
+        babyYoda.onUpdate(() => {
 
-        }
+            // Define the keyboard keys for movement
+            const MOVE_LEFT_KEY = "left";
+            const MOVE_RIGHT_KEY = "right";
+            const MOVE_UP_KEY = "up";
+            const MOVE_DOWN_KEY = "down";
+
+            const movementSpeed = 1;
+
+            // Handle keyboard input
+            onKeyDown(MOVE_LEFT_KEY, () => {
+                babyYoda.move(-movementSpeed, 0);
+            });
+
+            onKeyDown(MOVE_RIGHT_KEY, () => {
+                babyYoda.move(movementSpeed, 0);
+ 
+            });
+
+            onKeyDown(MOVE_UP_KEY, () => {
+                babyYoda.move(0, -movementSpeed);
+            });
+
+            onKeyDown(MOVE_DOWN_KEY, () => {
+                babyYoda.move(0, movementSpeed);
+            });
+        })
+    };
+    
+  export default spawnBabyYoda;
